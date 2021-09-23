d50f2c4d373e19f7ea87e6320c585246d54a933b
Yarema Serbin <mooyyapochta@gmail.com>
2021-09-23 19:56:58 +0100

    HW-1 added

--- HW-1/HW-1.js
+++ HW-1/HW-1.js
@@ -6,29 +6,27 @@ console.log("Максимальна ціна:"+Math.max(trousers,shoes,shirt));
 // 2 task
 console.log("Мінімальна ціна:"+Math.min(trousers,shoes,shirt));
 // 3 task
-const suma = trousers + shoes + shirt;
-console.log("Вартість всіх товарів:"+suma);
+const sum = trousers + shoes + shirt;
+console.log("Вартість всіх товарів:"+sum);
 // 4 task
- console.log ("Сума вартості без дробів:"+Math.floor (Math.trunc(trousers) + Math.trunc(shoes) + Math.trunc(shirt)));
+console.log ("Сума вартості без дробів:"+Math.floor (Math.trunc(trousers) + Math.trunc(shoes) + Math.trunc(shirt)));
 //  5 task
-console.log ("Сума товарів заокруглена до сотень:"+Math.round((suma)/100)*100);
+console.log ("Сума товарів заокруглена до сотень:"+Math.round((sum)/100)*100);
 //  6 task
-const booleanValue =  (Math.floor(trousers) + Math.floor(shoes) + Math.floor(shirt));
+const booleanValue =  Math.floor(trousers) + Math.floor(shoes) + Math.floor(shirt);
 console.log(booleanValue %2 === 0);
 // 7 task
 const cash = 500;
-console.log("Здача:"+(cash - suma));
+console.log("Здача:"+(cash - sum));
 // 8 task
-const averageValue = (suma / 3);
+const averageValue = (sum / 3);
 console.log("Середнє значення" +averageValue.toFixed(2));
 // 9 task
 const randomDiscount = Math.round(Math.random()*101);
 console.log("Випадкова знижка:" +randomDiscount);
 
-const payRandomDiscount = ((suma-suma * (randomDiscount / 100)).toFixed(2));
+const payRandomDiscount = +((sum-sum * (randomDiscount / 100)).toFixed(2));
 console.log ("Оплата зі знижкою:" +payRandomDiscount);
-const productionCost = (Math.round(suma)) / 2;
-const netprofit = (productionCost - payRandomDiscount).toFixed(2);
+const productionCost = (Math.round(sum)) / 2;
+const netprofit = +(productionCost - payRandomDiscount).toFixed(2);
 console.log("Чистий прибуток зі знижкою:" +netprofit);
-
-

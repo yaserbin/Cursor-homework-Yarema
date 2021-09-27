de13ddcfb966781a2ed07207801e5137aebacc01
Yarema Serbin <mooyyapochta@gmail.com>
2021-09-27 23:26:31 +0300

    HW-2 added

---
+++ HW-2/HW-2.js
@@ -0,0 +1,30 @@
+let n = "";
+
+do {
+    n = +prompt("Введіть ціле число n:", "");
+} while(isNaN(n) || n % 1 !==0 || n === "");
+document.writeln(n);
+
+let m = "";
+ 
+do {
+    m = +prompt("Введіть ціле число m:", "");
+} while(isNaN(m) || m % 1 !==0 || m === "");
+document.writeln(m);
+
+const evenNumbers = confirm("Пропускати парні числа?", "");
+
+let sum = 0;
+
+if (evenNumbers === true) {
+     for (let i = n; i <= m; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        };
+    };
+} else {
+        for (let i = n; i <= m; i++) {
+            sum += i;
+    };
+};
+document.writeln(sum);
